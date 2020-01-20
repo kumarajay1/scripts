189676d9c397a3495a81ab02b8de8ca5d6e1bdef
kumarajay1 <majaykumar.iiitbasar@gmail.com>
2020-01-20 16:58:13 +0530

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+echo "Login successfully"
+echo "Running sample shell script"
+sleep 2
+ifconfig
+sleep 2
+echo "ifconfig Completed"
+
+#trap 'trap - INT; kill -s HUP -- -$$' INT
+#ping www.google.com
+#ping www.google.com
+sleep 3
+
+#echo "Ping Complete"
+
+
+
+
